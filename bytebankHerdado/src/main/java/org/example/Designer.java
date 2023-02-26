15f9316db9c8e1f8a7c07b0b994741b316c95b07
doni-zete <donizete112@gmail.com>
2023-02-25 21:42:09 -0300

    mover

--- bytebankHerdado/src/main/java/org/example/Designer.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Designer extends Funcionario {
-
-    public double getBonificacao() {
-        System.out.println("Chamando o meu metodo de bonificação Designer");
-        return  200;
-    }
-}
--- bytebankHerdado/src/main/java/org/example/EditorVideo.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class EditorVideo extends Funcionario {
-
-    public double getBonificacao() {
-        System.out.println("Chamando o meu metodo de bonificação EV");
-        return super.getBonificacao() + 100;
-    }
-}
